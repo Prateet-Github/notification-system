9e8b64734dbaf631d81a842cf3e17023fde32e61
Prateet Tiwari <prateettiwari29@gmail.com>
2026-05-04 23:32:33 +0530

    feat: refactor application structure and add health check module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { HealthModule } from './health/health.module';
+import configuration from './config/env.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    HealthModule,
+
+  ]
 })
-export class AppModule {}
+export class AppModule { }
+
