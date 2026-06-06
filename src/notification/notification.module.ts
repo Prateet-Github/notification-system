5a0f944af0453d11a8ee2c23dc7c1b529c92a1bb
Prateet Tiwari <prateettiwari29@gmail.com>
2026-06-07 03:07:45 +0530

    feat(notification): enhance notification creation with dynamic delivery channels based on user preferences

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { PreferencesModule } from '../preferences/preferences.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PreferencesModule],
   controllers: [NotificationController],
   providers: [NotificationService],
 })
-export class NotificationModule {}
+export class NotificationModule { }
--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,37 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateNotificationDto } from './dto/create-notification.dto';
+import { Channel } from '@prisma/client';
 
 @Injectable()
 export class NotificationService {
   constructor(private prisma: PrismaService) { }
 
   async create(dto: CreateNotificationDto) {
+    const preference = await this.prisma.userPreference.findUnique({
+      where: {
+        userId: dto.userId,
+      },
+      include: {
+        channels: true,
+      },
+    });
+
+    const enabledChannels = preference
+      ? preference.channels
+        .filter((channel) => channel.isEnabled)
+        .map((channel) => ({
+          channel: channel.channel,
+        }))
+      : [
+        { channel: Channel.EMAIL },
+        { channel: Channel.PUSH },
+        { channel: Channel.IN_APP },
+      ];
+
     return this.prisma.notification.create({
       data: {
         userId: dto.userId,
         eventType: dto.eventType,
         priority: dto.priority,
         payload: dto.payload,
-        deliveries: {
-          create: [
-            { channel: 'EMAIL' },
-            { channel: 'SMS' },
-            { channel: 'PUSH' },
-            { channel: 'IN_APP' },
-          ]
-        }
-      },
-      include: {
-        deliveries: {
-          select: {
-            id: true,
-            channel: true,
-            status: true,
-          },
-        },
 
-      }
+        deliveries: {
+          create: enabledChannels,
+        },
+      },
+
+      include: {
+        deliveries: true,
+      },
     });
   }
 
