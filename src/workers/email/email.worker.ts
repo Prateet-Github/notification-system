8e2e4e37b18817e47e0e67ed040fe76deacd954f
Prateet Tiwari <prateettiwari29@gmail.com>
2026-06-10 15:50:11 +0530

    feat(workers): enhance EmailWorker to process delivery status updates

--- src/workers/email/email.worker.ts
+++ src/workers/email/email.worker.ts
@@ -1,10 +1,39 @@
 import { Processor, WorkerHost } from '@nestjs/bullmq';
+import { Injectable } from '@nestjs/common';
 import { Job } from 'bullmq';
-import { QUEUES } from '../../queue/constants';
 
+import { PrismaService } from '../../prisma/prisma.service';
+import { QUEUES } from '../../queue/constants';
+import { Status } from '@prisma/client';
+
+@Injectable()
 @Processor(QUEUES.EMAIL)
 export class EmailWorker extends WorkerHost {
-  async process(job: Job<any>): Promise<void> {
-    console.log('Email job received:', job.data);
+  constructor(
+    private readonly prisma: PrismaService,
+  ) {
+    super();
+  }
+
+  async process(job: Job<{ deliveryId: string }>): Promise<void> {
+    const { deliveryId } = job.data;
+
+    await this.prisma.delivery.update({
+      where: {
+        id: deliveryId,
+      },
+      data: {
+        status: Status.SENT,
+      },
+    });
+
+    const delivery = await this.prisma.delivery.findUnique({
+      where: {
+        id: deliveryId,
+      },
+    });
+
+    console.log('Email job received:', deliveryId);
+    console.log('Delivery:', delivery);
   }
 }
--- src/workers/workers.module.ts
+++ src/workers/workers.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EmailWorker } from './email/email.worker';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   providers: [EmailWorker],
+  imports: [PrismaModule],
 })
 export class WorkersModule { }
