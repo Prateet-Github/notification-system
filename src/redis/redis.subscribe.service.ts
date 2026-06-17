7b755d1661e59dbb49abffebe5c536e943da4522
Prateet Tiwari <prateettiwari29@gmail.com>
2026-06-17 14:26:59 +0530

    feat(redis): implement RedisSubscriberService with subscription to notifications channel and logging

--- src/redis/redis.subscribe.service.ts
+++ src/redis/redis.subscribe.service.ts
@@ -1,15 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Redis from 'ioredis';
 import { getRedisConfig } from '../config/redis.config';
 
 @Injectable()
-export class RedisSubscriberService {
+export class RedisSubscriberService implements OnModuleInit {
   private readonly redis: Redis;
+  private readonly logger = new Logger(RedisSubscriberService.name);
 
   constructor(private readonly configService: ConfigService) {
     this.redis = new Redis(
       getRedisConfig(this.configService),
+    );
+  }
+
+  async onModuleInit() {
+    await this.redis.subscribe('notifications');
+
+    this.logger.log(
+      'Subscribed to notifications channel',
+    );
+
+    this.redis.on(
+      'message',
+      (channel, message) => {
+        this.logger.log(
+          `[${channel}] ${message}`,
+        );
+      },
     );
   }
 }
