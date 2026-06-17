8e5dea2f3b95ae95224802595324af3adef93db1
Prateet Tiwari <prateettiwari29@gmail.com>
2026-06-17 17:46:53 +0530

    feat(redis): integrate SseModule and update RedisSubscriberService to publish events

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, Sse } from '@nestjs/common';
 import { RedisPublisherService } from './redis.publish.service';
 import { RedisSubscriberService } from './redis.subscribe.service';
+import { SseModule } from '@/sse/sse.module';
 
 @Module({
+  imports: [SseModule],
   providers: [RedisPublisherService, RedisSubscriberService],
   exports: [RedisPublisherService, RedisSubscriberService],
 })
--- src/redis/redis.subscribe.service.ts
+++ src/redis/redis.subscribe.service.ts
@@ -2,13 +2,17 @@ import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Redis from 'ioredis';
 import { getRedisConfig } from '../config/redis.config';
+import { SseService } from '@/sse/sse.service';
 
 @Injectable()
 export class RedisSubscriberService implements OnModuleInit {
   private readonly redis: Redis;
   private readonly logger = new Logger(RedisSubscriberService.name);
 
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly sseService: SseService,
+  ) {
     this.redis = new Redis(
       getRedisConfig(this.configService),
     );
@@ -21,11 +25,26 @@ export class RedisSubscriberService implements OnModuleInit {
       'Subscribed to notifications channel',
     );
 
+    // this.redis.on(
+    //   'message',
+    //   (channel, message) => {
+    //     this.logger.log(
+    //       `[${channel}] ${message}`,
+    //     );
+    //   },
+    // );
     this.redis.on(
       'message',
       (channel, message) => {
         this.logger.log(
           `[${channel}] ${message}`,
+        );
+
+        const event = JSON.parse(message);
+
+        this.sseService.publish(
+          event.userId,
+          event.payload,
         );
       },
     );
